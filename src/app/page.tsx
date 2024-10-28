6c8be534a2884f662fe5ccce4c4b7bf926288250
Leandro Amorim <leandroamorimdev@gmail.com>
2024-10-28 02:32:45 -0300

    Limit width to 1080p

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,7 @@ const Footer = lazy(() => import("./components/footer/Footer"));
 
 export default function Home() {
   return (
-    <div className="flex flex-col w-full min-h-screen relative overflow-hidden">
+    <div className="flex flex-col w-full max-w-[1920px] mx-auto min-h-screen relative overflow-hidden">
       <Header />
       <main className="flex flex-col">
         <HeroSection />
